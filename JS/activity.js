1510aa128f73f30ff4e724fccc52c4bc9cfb73ba
Boneva <lixinyue@gmail.com>
2024-06-11 18:46:31 -0700

    code add in

--- JS/activity.js
+++ JS/activity.js
@@ -1,9 +1,21 @@
-$("td#pid").removeClass("selectable");
-
 $(document).ready(function() {
-$("td#pid").click(function() {
-    $(this).toggleClass("selectable");
-});
+    $("td.selectable").click(function() {
+        var content = $(this).text();
 
-console.log("Script loaded successfully and ready.");
+        if (content != "Not Available") {
+            $(this).toggleClass("selected");
+
+            if ($(this).hasClass("selected")) {
+                $('#displaySelected').css("visibility", "visible");
+                $('#displaySelected').css("margin-top", "2em");
+                $('#result').append("<p>" + content + "</p>");
+            } else {
+                $('#result p:contains(' + content + ')').remove();
+                if ($('#result').has('p').length == false) {
+                    $('#displaySelected').css("visibility", "hidden");
+                    $('#displaySelected').css("margin-top", "0");
+                }
+            }
+        }
+    });
 });
